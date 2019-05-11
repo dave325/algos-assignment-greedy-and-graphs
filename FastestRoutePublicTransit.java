eeec83009f7e0319eb8b70dfcbe2f19fcc0c2929
David <davedataram@gmail.com>
2019-05-11 09:51:25 -0400

    Added writeup and created extra functions

--- FastestRoutePublicTransit.java
+++ FastestRoutePublicTransit.java
@@ -1,47 +1,165 @@
 /**
  * Public Transit
- * Author: Your Name and Carolyn Yao
+ * Author: David Dataram and Carolyn Yao
  * Does this compile? Y/N
  */
 
 /**
  * This class contains solutions to the Public, Public Transit problem in the
  * shortestTimeToTravelTo method. There is an existing implementation of a
- * shortest-paths algorithm. As it is, you can run this class and get the solutions
- * from the existing shortest-path algorithm.
+ * shortest-paths algorithm. As it is, you can run this class and get the
+ * solutions from the existing shortest-path algorithm.
  */
 public class FastestRoutePublicTransit {
 
   /**
-   * The algorithm that could solve for shortest travel time from a station S
-   * to a station T given various tables of information about each edge (u,v)
+   * The algorithm that could solve for shortest travel time from a station S to a
+   * station T given various tables of information about each edge (u,v)
    *
-   * @param S the s th vertex/station in the transit map, start From
-   * @param T the t th vertex/station in the transit map, end at
+   * @param S         the s th vertex/station in the transit map, start From
+   * @param T         the t th vertex/station in the transit map, end at
    * @param startTime the start time in terms of number of minutes from 5:30am
-   * @param lengths lengths[u][v] The time it takes for a train to get between two adjacent stations u and v
-   * @param first first[u][v] The time of the first train that stops at u on its way to v, int in minutes from 5:30am
-   * @param freq freq[u][v] How frequently is the train that stops at u on its way to v
+   * @param lengths   lengths[u][v] The time it takes for a train to get between
+   *                  two adjacent stations u and v
+   * @param first     first[u][v] The time of the first train that stops at u on
+   *                  its way to v, int in minutes from 5:30am
+   * @param freq      freq[u][v] How frequently is the train that stops at u on
+   *                  its way to v
    * @return shortest travel time between S and T
    */
-  public int myShortestTravelTime(
-    int S,
-    int T,
-    int startTime,
-    int[][] lengths,
-    int[][] first,
-    int[][] freq
-  ) {
+  public void myShortestTravelTime(int S, int T, int startTime, int[][] lengths, int[][] first, int[][] freq) {
     // Your code along with comments here. Feel free to borrow code from any
     // of the existing method. You can also make new helper methods.
-    return 0;
+    int numVertices = lengths[0].length;
+
+    // This is the array where we'll store all the final shortest times
+    int[] times = new int[numVertices];
+
+    // processed[i] will true if vertex i's shortest time is already finalized
+    Boolean[] processed = new Boolean[numVertices];
+
+    // Initialize all distances as INFINITE and processed[] as false
+    for (int v = 0; v < numVertices; v++) {
+      times[v] = Integer.MAX_VALUE;
+      processed[v] = false;
+    }
+
+    // Distance of source vertex from itself is always 0
+    times[S] = 0;
+    //The total Minutes after each vertice is relax 
+    int totalMin = startTime;
+    // Failsafe in case the destination oes not exist 
+    if(numVertices <= T){
+      System.out.println("T does not exist.");
+      return;
+    }
+    // Find shortest path to all the vertices
+    for (int count = 0; count < numVertices - 1; count++) {
+      // Pick the minimum distance vertex from the set of vertices not yet processed.
+      // u is always equal to source in first iteration.
+      // Mark u as processed.
+      int u = findNextToProcess(times, processed);
+      processed[u] = true;
+      // Temp - Stores the calculations of all edges 
+      // Min - Stores the collective minimum value for each edge
+      int temp = 0, min = Integer.MAX_VALUE;
+      // Update time value of all the adjacent vertices of the picked vertex.
+      for (int v = 0; v < numVertices; v++) {
+        // Update time[v] only if is not processed yet, there is an edge from u to v,
+        // and total weight of path from source to v through u is smaller than current
+        // value of time[v]
+        //System.out.println(u + ": " +totalMin);
+        //Returns the value for each edge in the case of first, freq, and length
+        temp = findFullValue(totalMin, processed, u, v, lengths, first, freq);
+        //System.out.println("Final - " + u + " - " + v + ": " + temp);
+        if (!processed[v] && lengths[u][v] != 0 && times[u] != Integer.MAX_VALUE && temp != Integer.MIN_VALUE && temp < times[v]) {
+
+          times[v] = temp;
+          // If the temp value is greater than the min value, store the numbers
+          if (min > temp) {
+            min = temp;
+          }
+        }
+      }
+      if(min == Integer.MAX_VALUE){
+        min = 0;
+      }
+      // Increment the totalMin by the collective min value from each vertice
+      totalMin += min;
+    }
+    System.out.println("Station " + S + " to Station " + T);
+    printShortestTimes(times, times[T]);
+  }
+
+  /**
+   * 
+   * @param time
+   * @param processed
+   * @param u
+   * @param v
+   * @param lengths
+   * @param first
+   * @param freq
+   * @return int time
+   */
+  public int findFullValue(int time, Boolean[] processed, int u, int v, int[][] lengths, int[][] first, int[][] freq) {
+    int min = 0;
+    // int minIndex = -1;
+    int temp = 0;
+    //System.out.println("\nVertice: " + u + " to " + v + "\n");
+    min = Integer.MAX_VALUE;
+    //System.out.println("Time: " + time);
+    if (u != v) {
+      // for (int i = 0; i < lengths[u].length; i++) {
+      // System.out.println("First: " + v + ": " + first[u][v]);
+      //System.out.println("Lengths: " + v + ": " + lengths[u][v]);
+      // Check if an edge exists and the destination has not been processed
+      if (lengths[u][v] > 0 && processed[v] == false) {
+        // Store the first occurance from u to v
+        temp = first[u][v];
+        // If the first occurance is before the current time, 
+        // add onto the temp time until the next train comes,
+        // based on the frequency 
+        while (time > temp) {
+          temp += freq[u][v];
+        }
+        //System.out.println("Temp(" + v + ") - " + temp);
+       
+        /**
+         * If the min time is greater than the difference between the
+         * temp and the current minute plus the length
+         * Store the temp time 
+         */
+        if (min > (temp - time) + lengths[u][v]) {
+          min = (temp - time) + lengths[u][v];
+        }
+        //System.out.println("Min(" + v + ") - " + min);
+      }else{
+        min = 0;
+      }
+      // System.out.println("Min value: " + min);
+      /*
+       * if (processed[i] == false && times[i] <= min) { min = times[i]; minIndex = i;
+       * }
+       */
+      // }
+    } else {
+      min = 0;
+    }
+    // Increment the current time to the min time
+    time += min;
+     //System.out.println("Time: " + time);
+    // System.out.println("Lengths - " + min);
+    return time;
   }
 
   /**
    * Finds the vertex with the minimum time from the source that has not been
    * processed yet.
-   * @param times The shortest times from the source
-   * @param processed boolean array tells you which vertices have been fully processed
+   * 
+   * @param times     The shortest times from the source
+   * @param processed boolean array tells you which vertices have been fully
+   *                  processed
    * @return the index of the vertex that is next vertex to process
    */
   public int findNextToProcess(int[] times, Boolean[] processed) {
@@ -57,16 +175,22 @@ public class FastestRoutePublicTransit {
     return minIndex;
   }
 
-  public void printShortestTimes(int times[]) {
+  public void printShortestTimes(int times[], int totalTime) {
     System.out.println("Vertex Distances (time) from Source");
+    System.out.println("Total Time: " + totalTime);
     for (int i = 0; i < times.length; i++)
-        System.out.println(i + ": " + times[i] + " minutes");
+      System.out.println(i + ": " + times[i] + " minutes");
+
+    if (totalTime > 0) {
+      System.out.println("Total Time: " + totalTime);
+    }
   }
 
   /**
    * Given an adjacency matrix of a graph, implements
-   * @param graph The connected, directed graph in an adjacency matrix where
-   *              if graph[i][j] != 0 there is an edge with the weight graph[i][j]
+   * 
+   * @param graph  The connected, directed graph in an adjacency matrix where if
+   *               graph[i][j] != 0 there is an edge with the weight graph[i][j]
    * @param source The starting vertex
    */
   public void shortestTime(int graph[][], int source) {
@@ -88,7 +212,7 @@ public class FastestRoutePublicTransit {
     times[source] = 0;
 
     // Find shortest path to all the vertices
-    for (int count = 0; count < numVertices - 1 ; count++) {
+    for (int count = 0; count < numVertices - 1; count++) {
       // Pick the minimum distance vertex from the set of vertices not yet processed.
       // u is always equal to source in first iteration.
       // Mark u as processed.
@@ -98,31 +222,31 @@ public class FastestRoutePublicTransit {
       // Update time value of all the adjacent vertices of the picked vertex.
       for (int v = 0; v < numVertices; v++) {
         // Update time[v] only if is not processed yet, there is an edge from u to v,
-        // and total weight of path from source to v through u is smaller than current value of time[v]
-        if (!processed[v] && graph[u][v]!=0 && times[u] != Integer.MAX_VALUE && times[u]+graph[u][v] < times[v]) {
+        // and total weight of path from source to v through u is smaller than current
+        // value of time[v]
+        if (!processed[v] && graph[u][v] != 0 && times[u] != Integer.MAX_VALUE && times[u] + graph[u][v] < times[v]) {
           times[v] = times[u] + graph[u][v];
         }
       }
     }
 
-    printShortestTimes(times);
+    printShortestTimes(times, 0);
   }
 
-  public static void main (String[] args) {
+  public static void main(String[] args) {
     /* length(e) */
-    int lengthTimeGraph[][] = new int[][]{
-      {0, 4, 0, 0, 0, 0, 0, 8, 0},
-      {4, 0, 8, 0, 0, 0, 0, 11, 0},
-      {0, 8, 0, 7, 0, 4, 0, 0, 2},
-      {0, 0, 7, 0, 9, 14, 0, 0, 0},
-      {0, 0, 0, 9, 0, 10, 0, 0, 0},
-      {0, 0, 4, 14, 10, 0, 2, 0, 0},
-      {0, 0, 0, 0, 0, 2, 0, 1, 6},
-      {8, 11, 0, 0, 0, 0, 1, 0, 7},
-      {0, 0, 2, 0, 0, 0, 6, 7, 0}
-    };
+    int lengthTimeGraph[][] = new int[][] { { 0, 4, 0, 0, 0, 0, 0, 8, 0 }, { 4, 0, 8, 0, 0, 0, 0, 11, 0 },
+        { 0, 8, 0, 7, 0, 4, 0, 0, 2 }, { 0, 0, 7, 0, 9, 14, 0, 0, 0 }, { 0, 0, 0, 9, 0, 10, 0, 0, 0 },
+        { 0, 0, 4, 14, 10, 0, 2, 0, 0 }, { 0, 0, 0, 0, 0, 2, 0, 1, 6 }, { 8, 11, 0, 0, 0, 0, 1, 0, 7 },
+        { 0, 0, 2, 0, 0, 0, 6, 7, 0 } };
+
+    int newLength[][] = new int[][] { { 0, 5, 1 }, { 0, 0, 5 }, { 0, 0, 1 } };
+
+    int firstTimeGraph[][] = new int[][] { { 0, 3, 10 }, { 0, 0, 5 }, { 0, 0, 10 } };
+    int freqTimeGraph[][] = new int[][] { { 0, 2, 10 }, { 0, 0, 3 }, { 0, 0, 10 } };
     FastestRoutePublicTransit t = new FastestRoutePublicTransit();
-    t.shortestTime(lengthTimeGraph, 0);
+    // t.shortestTime(lengthTimeGraph, 0);
+    t.myShortestTravelTime(0, 2, 0, newLength, firstTimeGraph, freqTimeGraph);
 
     // You can create a test case for your implemented method for extra credit below
   }
--- PhysicsExperiment.java
+++ PhysicsExperiment.java
@@ -1,60 +1,195 @@
 /**
  * Physics Experiment
- * Author: Your Name and Carolyn Yao
+ * Author: Davi Dataram and Carolyn Yao
  * Does this compile or finish running within 5 seconds? Y/N
  */
 
 /**
- * This class implements a greedy scheduler to assign students
- * to steps in a physics experiment. There are three test cases in the main
- * method. Please read through the whole file before attempting to code the
- * solution.
+ * This class implements a greedy scheduler to assign students to steps in a
+ * physics experiment. There are three test cases in the main method. Please
+ * read through the whole file before attempting to code the solution.
  *
- * You will only be graded on code you add to the scheduleExperiments method.
- * Do not mess with the existing formatting and identation.
- * You don't need to use the helper methods, but if they come in handy setting
- * up a custom test case, feel free to use them.
+ * You will only be graded on code you add to the scheduleExperiments method. Do
+ * not mess with the existing formatting and identation. You don't need to use
+ * the helper methods, but if they come in handy setting up a custom test case,
+ * feel free to use them.
  */
 public class PhysicsExperiment {
 
   /**
    * The actual greedy scheduler you will be implementing!
+   * 
    * @param numStudents The number of students who can participate, m
-   * @param numSteps The number of steps in the experiment, n
-   * @param signUpTable An easy lookup tool, signUpTable[x][Y] = student X signed up or did not sign up for step Y.
-   *      Example:
-          signUpTable[1][3] = 1 if Student 1 signed up for Step 3
-          signUpTable[1][3] = 0 if Student 1 didn't sign up for Step 3
-   * @return scheduleTable: a table similar to the signUpTable where scheduleTable[X][Y] = 1 means
-   *     student X is assigned to step Y in an optimal schedule
+   * @param numSteps    The number of steps in the experiment, n
+   * @param signUpTable An easy lookup tool, signUpTable[x][Y] = student X signed
+   *                    up or did not sign up for step Y. Example:
+   *                    signUpTable[1][3] = 1 if Student 1 signed up for Step 3
+   *                    signUpTable[1][3] = 0 if Student 1 didn't sign up for Step
+   *                    3
+   * @return scheduleTable: a table similar to the signUpTable where
+   *         scheduleTable[X][Y] = 1 means student X is assigned to step Y in an
+   *         optimal schedule
    */
-  public int[][] scheduleExperiments(
-    int numStudents,
-    int numSteps,
-    int[][] signUpTable
-  ) {
+  public int[][] scheduleExperiments(int numStudents, int numSteps, int[][] signUpTable) {
     // Your scheduleTable is initialized as all 0's so far. Your code will put 1's
     // in the table in the right places based on the return description
     int[][] scheduleTable = new int[numStudents + 1][numSteps + 1];
-
+    // Holds the value of every student through each iteration
+    int consecutiveSteps[] = new int[numStudents + 1];
+    for (int i = 0; i < consecutiveSteps.length; i++) {
+      consecutiveSteps[i] = 0;
+    }
+    // Array to hold list of students and whether they were already selected
+    boolean[] studentWasUsed = new boolean[numStudents + 1];
+    // Value for students
+    int students = 0;
+    // Index that will hold which student can take the step
+    int validStudent = -1;
     // Your code goes here
+    // Array to loop through each step
+    for (int i = 1; i <= numSteps; i++) {
+      /*
+       * int validStudent = findStudentStep(consecutiveSteps, i);
+       * System.out.println(validStudent); if (consecutiveSteps[validStudent] > 0) {
+       * scheduleTable[validStudent][i] = 1; consecutiveSteps[validStudent]--;
+       * continue; } else {
+       */
+
+      // Check if there is a valid student set
+      // Check if the current index has nother iteration
+      if (validStudent > 0 && consecutiveSteps[validStudent] > 0) {
+
+        // System.out.println("Step: " + (i));
+        // System.out.println();
+        // System.out.println((consecutiveSteps[validStudent]));
+        // Set the scheduleTable to 1
+        scheduleTable[validStudent][i] = 1;
+        // Reduce the consecutive count by 1
+        consecutiveSteps[validStudent]--;
+        // System.out.println(i);
+        // Run once the no more consective students are avilable
+        // Reset all of the consecutive array
+        if (consecutiveSteps[validStudent] == 0) {
+          for (int o = 0; o < consecutiveSteps.length; o++) {
+            consecutiveSteps[o] = 0;
+          }
+        }
+        // Ignore rest of for loop
+        continue;
+      }
+      // Loop through list of students
+      for (students = 1; students <= numStudents; students++) {
+        // Check if student is elligible to take current step
+        if (studentHasStep(signUpTable, students, i)) {
+          // If the student can take the step increase counter
+          consecutiveSteps[students]++;
+          // Loop through the remaining steps
+          innerloop: for (int temp = i; temp <= numSteps; temp++) {
+            // Check if student has the next step of the iteratino and if they hold the
+            // current step
+            if (studentHasNextStep(signUpTable, students, temp) && studentHasStep(signUpTable, students, i)) {
+              // Increas counter for that student
+              consecutiveSteps[students]++;
+              continue;
+            } else {
+              // If they do not have the next step in loop or current step, break the loop
+              break innerloop;
+            }
+          } // End of temp numSteps for loop
+        } // End of studentHasStep() && studentDidNotGo() if statement
+      } // End of students for loop
+      // Retrieve the optimal index from loop
+      validStudent = findStudentStep(consecutiveSteps, studentWasUsed);
+      // Store the student variable in array to know they went already
+      studentWasUsed[validStudent] = true;
+      // Update the table
+      scheduleTable[validStudent][i] = 1;
+      // Reduce counter by 1
+      consecutiveSteps[validStudent]--;
+    }
+    // } // End of numSteps for loop
 
     return scheduleTable;
   }
 
   /**
-   * Makes the convenient lookup table based on the steps each student says they can do
-   * @param numSteps the number of steps in the experiment
+   * 
+   * @param signUpTable Current table that holds whether students can do the step
+   * @param students    Current Student
+   * @param step        Current step
+   * @param totalSteps
+   * @return boolean
+   */
+  private boolean studentHasStep(int[][] signUpTable, int students, int step) {
+    if (signUpTable[students][step] == 1) {
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * 
+   * @param arr         Array of current students
+   * @param alreadyWent Array of students containing boolean variable
+   * @return index of student
+   */
+  private int findStudentStep(int[] arr, boolean[] alreadyWent) {
+    // Variable to hold current value
+    int temp = -1;
+    // Variable to hold index
+    int result = -1;
+    // Variable that checks if there is a user who already went
+    boolean set = false;
+    // Loop through consecutive array
+    for (int i = 0; i < arr.length; i++) {
+      // Store value of arr at the index
+      int num = arr[i];
+      // Check if current user already went and add the info to the array
+      if (alreadyWent[i] && num > temp) {
+        set = true;
+        result = i;
+        temp = num;
+      }
+      if (!set && num > temp) {
+        result = i;
+        temp = num;
+      }
+      set = false;
+    }
+
+    return result;
+  }
+
+  private boolean studentDidGo() {
+    return true;
+  }
+
+  private boolean studentHasNextStep(int[][] signUpTable, int students, int step) {
+    try {
+      if (signUpTable[students][step + 1] == 1) {
+        return true;
+      }
+      return false;
+    } catch (ArrayIndexOutOfBoundsException ae) {
+      return false;
+    }
+  }
+
+  /**
+   * Makes the convenient lookup table based on the steps each student says they
+   * can do
+   * 
+   * @param numSteps       the number of steps in the experiment
    * @param studentSignUps student sign ups ex: {{1, 2, 4}, {3, 5}, {6, 7}}
    * @return a lookup table so if we want to know if student x can do step y,
-      lookupTable[x][y] = 1 if student x can do step y
-      lookupTable[x][y] = 0 if student x cannot do step y
+   *         lookupTable[x][y] = 1 if student x can do step y lookupTable[x][y] =
+   *         0 if student x cannot do step y
    */
   public int[][] makeSignUpLookup(int numSteps, int[][] studentSignUps) {
     int numStudents = studentSignUps.length;
-    int[][] lookupTable = new int[numStudents+1][numSteps + 1];
+    int[][] lookupTable = new int[numStudents + 1][numSteps + 1];
     for (int student = 1; student <= numStudents; student++) {
-      int[] signedUpSteps = studentSignUps[student-1];
+      int[] signedUpSteps = studentSignUps[student - 1];
       for (int i = 0; i < signedUpSteps.length; i++) {
         lookupTable[student][signedUpSteps[i]] = 1;
       }
@@ -65,8 +200,10 @@ public class PhysicsExperiment {
   /**
    * Prints the optimal schedule by listing which steps each student will do
    * Example output is Student 1: 1, 3, 4
+   * 
    * @param schedule The table of 0's and 1's of the optimal schedule, where
-   *   schedule[x][y] means whether in the optimal schedule student x is doing step y
+   *                 schedule[x][y] means whether in the optimal schedule student
+   *                 x is doing step y
    */
   public void printResults(int[][] schedule) {
     for (int student = 1; student < schedule.length; student++) {
@@ -83,12 +220,13 @@ public class PhysicsExperiment {
 
   /**
    * This validates the input data about the experiment step sign-ups.
+   * 
    * @param numStudents the number of students
-   * @param numSteps the number of steps
-   * @param signUps the data given about which steps each student can do
+   * @param numSteps    the number of steps
+   * @param signUps     the data given about which steps each student can do
    * @return true or false whether the input sign-ups match the given number of
-   *    students and steps, and whether all the steps are guaranteed at least
-   *    one student.
+   *         students and steps, and whether all the steps are guaranteed at least
+   *         one student.
    */
   public boolean inputsValid(int numStudents, int numSteps, int signUps[][]) {
     int studentSignUps = signUps.length;
@@ -124,9 +262,11 @@ public class PhysicsExperiment {
 
   /**
    * This sets up the scheduling test case and calls the scheduling method.
+   * 
    * @param numStudents the number of students
-   * @param numSteps the number of steps
-   * @param signUps which steps each student can do, in order of students and steps
+   * @param numSteps    the number of steps
+   * @param signUps     which steps each student can do, in order of students and
+   *                    steps
    */
   public void makeExperimentAndSchedule(int experimentNum, int numStudents, int numSteps, int[][] signUps) {
     System.out.println("----Experiment " + experimentNum + "----");
@@ -141,25 +281,26 @@ public class PhysicsExperiment {
   }
 
   /**
-   * You can make additional test cases using the same format. In fact the helper functions
-   * I've provided will even check your test case is set up correctly. Do not touch any of
-   * of the existing lines. Just make sure to comment out or delete any of your own test cases
-   * when you submit. The three experiment test cases existing in this main method should be
-   * the only output when running this file.
+   * You can make additional test cases using the same format. In fact the helper
+   * functions I've provided will even check your test case is set up correctly.
+   * Do not touch any of of the existing lines. Just make sure to comment out or
+   * delete any of your own test cases when you submit. The three experiment test
+   * cases existing in this main method should be the only output when running
+   * this file.
    */
-  public static void main(String args[]){
+  public static void main(String args[]) {
     PhysicsExperiment pe = new PhysicsExperiment();
 
     // Experiment 1: Example 1 from README, 3 students, 6 steps:
-    int[][] signUpsExperiment1 = {{1, 2, 3, 5}, {2, 3, 4}, {1, 4, 5, 6}};
+    int[][] signUpsExperiment1 = { { 1, 2, 3, 5 }, { 2, 3, 4 }, { 1, 4, 5, 6 } };
     pe.makeExperimentAndSchedule(1, 3, 6, signUpsExperiment1);
 
     // Experiment 2: Example 2 from README, 4 students, 8 steps
-    int[][] signUpsExperiment2 = {{5, 7, 8}, {2, 3, 4, 5, 6}, {1, 5, 7, 8}, {1, 3, 4, 8}};
+    int[][] signUpsExperiment2 = { { 5, 7, 8 }, { 2, 3, 4, 5, 6 }, { 1, 5, 7, 8 }, { 1, 3, 4, 8 } };
     pe.makeExperimentAndSchedule(2, 4, 8, signUpsExperiment2);
 
     // Experiment 3: Another test case, 5 students, 11 steps
-    int[][] signUpsExperiment3 = {{7, 10, 11}, {8, 9, 10}, {2, 3, 4, 5, 7}, {1, 5, 6, 7, 8}, {1, 3, 4, 8}};
+    int[][] signUpsExperiment3 = { { 7, 10, 11 }, { 8, 9, 10 }, { 2, 3, 4, 5, 7 }, { 1, 5, 6, 7, 8 }, { 1, 3, 4, 8 } };
     pe.makeExperimentAndSchedule(3, 5, 11, signUpsExperiment3);
   }
 }
